c0de45a2f6589a4260bad4d95a2c6227189f08a8
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-02 20:17:56 +0000

    refactor: Create a component for navigation to replace the navigation set up in the App.js

--- App.js
+++ App.js
@@ -4,29 +4,19 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
 import CartScreen from './CartScreen';
-
-const Drawer = createDrawerNavigator()
+import Navigation from './components/navigations';
 export default function App() {
   return (
     <NavigationContainer>
-    <Drawer.Navigator initialRouteName="HomeScreen">
-      <Drawer.Screen 
-      name="HomeScreen" 
-      component={HomeScreen} 
-      options = {{
-        headerShown: true
-      }}
-        />
-      <Drawer.Screen name="CartScreen" component={CartScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+      <Navigation/>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
