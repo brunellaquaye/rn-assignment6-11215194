52e805be09078b3228d7b4fc0e164c0818c36497
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-02 20:27:07 +0000

    feat: Create component to display items to be  checkout for the CartScreen.

---
+++ components/Checkout.js
@@ -0,0 +1,62 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React from "react";
+
+const CartsClothes = ({ image, title, description, price, icon }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <Image source={image} />
+      </View>
+      <View style={styles.text}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.amount}>{price}</Text>
+        <View style={styles.icons}>
+        
+          <Image source={icon} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    // marginBottom: 15,
+    marginLeft: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    position: "relative",
+    marginBottom: 15,
+    marginTop: 30,
+   
+  },
+  title: {
+    fontWeight: "400",
+    letterSpacing: 2,
+    fontSize: 20,
+  },
+  description: {
+    fontSize: 16,
+    marginTop:10
+},
+  amount: {
+    color: "brown",
+    marginTop: 10
+  },
+  icons: {
+    marginTop:40,
+    marginRight:20,
+    alignItems: "flex-end",
+  },
+  text: {
+    marginRight: 30,
+    marginLeft: 10,
+  },
+  icon: {},
+});
+
+export default CartsClothes;
