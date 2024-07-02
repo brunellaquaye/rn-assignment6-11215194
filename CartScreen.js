ababcc3d9087504f637e34efa245c89b76a5624e
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-02 20:29:09 +0000

    Implement Cartscreen features to display items to be schecked out and the drawer navigation icon.

--- CartScreen.js
+++ CartScreen.js
@@ -1,12 +1,165 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import CartsClothes from "./components/Checkout";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import {useNavigation} from "@react-navigation/native";
 const CartScreen = () => {
+    const navigation = useNavigation();
+  const clothesData = [
+    {
+      id: 1,
+      image: require("./assets/dress1.png"),
+      title: "OFFICE WEAR",
+      description: "Office wear for your Office",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 2,
+      image: require("./assets/dress2.png"),
+      title: "BLACK",
+      description: "Recycle Knit Cardigan ",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 3,
+      image: require("./assets/dress3.png"),
+      title: "CHURCH WEAR",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 4,
+      image: require("./assets/dress4.png"),
+      title: " LAMEREI",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 5,
+      image: require("./assets/dress5.png"),
+      title: " 21WN",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 6,
+      image: require("./assets/dress6.png"),
+      title: " LOPO",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 7,
+      image: require("./assets/dress7.png"),
+      title: "21WN",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+    {
+      id: 8,
+      image: require("./assets/dress6.png"),
+      title: "LAME",
+      description: "Recycle Knit Cardigan",
+      price: "$120",
+      icon: require("./assets/remove.png"),
+    },
+  ];
   return (
-    <View>
-      <Text>CartScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style = {{backgroundColor:"white"}}>
+        <View style = {styles.header}>
+        <TouchableOpacity>
+        <Image style={styles.drawerMenu} source={require("./assets/Menu.png")} />
 
-export default CartScreen
+        </TouchableOpacity>
+          <Image style={styles.logo} source={require("./assets/Logo.png")} />
+          <Ionicons name="search-outline" size={24} color="black" style={{marginTop:40, marginRight: 30}} />
+        </View>
+
+        <Text style={styles.title}>CHECKOUT</Text>
+        <View style={styles.span}>
+          <View style={styles.line}></View>
+          <MaterialCommunityIcons
+            name="cards-diamond-outline"
+            size={20}
+            color="#AFB0B6"
+          />
+          <View style={styles.line}></View>
+        </View>
+        <View>
+          <FlatList
+            data={clothesData}
+            vertical
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <CartsClothes
+                image={item.image}
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+              />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "light",
+    marginTop: 20,
+    marginLeft: 110,
+    letterSpacing: 5,
+    justifyCOntent: "center",
+    alignContent: "center",
+  },
+  drawerMenu: {
+    width: 20,
+    height: 20,
+    margin: 10,
+    marginTop: 40,
+    marginLeft: 20,
+  },
+  line: {
+    width: 60,
+    height: 1,
+    backgroundColor: "#AFB0B6",
+    marginTop: 20,
+    alignContent: "center",
+    bottom: 10,
+  },
+  span: {
+    flexDirection: "row",
+    marginHorizontal: 120,
+  },
+  logo: {
+    marginLeft: 10,
+    marginTop: 40,
+  },
+  header:{
+    flexDirection:"row",
+    justifyContent: "space-between"
+  }
+});
+
+export default CartScreen;
