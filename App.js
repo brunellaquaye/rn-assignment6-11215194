7de3c9c1df44f2b0972affebd02ad9576fe8740c
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-03 19:56:05 +0000

    Wrap the app in the cart provider to ensure all the component get access to the functionalities in there.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './components/navigations';
+import { CartProvider } from './components/CartContext';
 export default function App() {
   return (
+    <CartProvider>
     <NavigationContainer>
       <Navigation/>
       </NavigationContainer>
+      </CartProvider>
   );
 }
 
